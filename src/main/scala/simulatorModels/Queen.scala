7dc4d4bb9fed2e5a366083922c53b09f74480363
Tabish <tabish.shaikh@springernature.com>
2019-06-13 13:20:34 +0530

    Added Functionality for Queen

--- src/main/scala/simulatorModels/Queen.scala
+++ src/main/scala/simulatorModels/Queen.scala
@@ -1,5 +1,9 @@
 package simulatorModels
 
 class Queen extends Piece {
-  override def move(initialPosition: Cell, board: ChessBoard): List[Cell] = List.empty
+  override def move(initialPosition: Cell, board: ChessBoard): List[Cell] = {
+    val bishopMoves = new Bishop().move(initialPosition, board)
+    val rookMoves = new Rook().move(initialPosition, board)
+    (bishopMoves ++ rookMoves)
+  }
 }
--- src/test/scala/simulatorModels/QueenTest.scala
+++ src/test/scala/simulatorModels/QueenTest.scala
@@ -1,18 +1,52 @@
 package simulatorModels
 
 import org.scalatestplus.play.PlaySpec
+import util.TestHelpers
 
-class QueenTest extends PlaySpec {
+class QueenTest extends PlaySpec with TestHelpers {
   "Queen" should {
-    "Move in vertically, Horizontally and diagonally in all directions" in {
+    "be able to move in all eight Diagonal, Vertical and  Horizontal directions when it has cells all around" in {
       val queen = new Queen()
-      val expectedList = (
-        "D1, D2, D3, D4, D6, D7, D8, " +
-          "A5, B5, C5, E5, F5, G5, H5, " +
-          "A1, B2, C3, D4, E5, F6, G7, H8, " +
-          "H1, G2, F3, E4, C6, B7, A8")
-        .split(", ").map(Cell(_).get).toList
-      queen.move(Cell('D', 5), new ChessBoard()) mustEqual expectedList
+      val expectedSequence = "F6, D4, G7, C3, H8, B2, A1, F4, D6, G3, C7, H2, B8, E1, E2, E3, E4, E6, E7, E8, A5, B5, C5, D5, F5, G5, H5"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('E', 5), new ChessBoard) mustEqual (expectedMoves)
+    }
+
+    "be able to move only up right Diagonally and bottom and left boundary when it is placed at bottom left corner" in {
+      val queen = new Queen()
+      val expectedSequence = "B2, C3, D4, E5, F6, G7, H8, A2, A3, A4, A5, A6, A7, A8, B1, C1, D1, E1, F1, G1, H1"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('A', 1), new ChessBoard) mustEqual (expectedMoves)
+    }
+    "be able to move only up left Diagonally and bottom and right boundary  when it is placed at bottom right corner" in {
+      val queen = new Queen()
+      val expectedSequence = "G2, F3, E4, D5, C6, B7, A8, H2, H3, H4, H5, H6, H7, H8, A1, B1, C1, D1, E1, F1, G1"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('H', 1), new ChessBoard) mustEqual (expectedMoves)
+    }
+    "be able to move only down left Diagonally and top and right boundary  when it is placed at right top corner" in {
+      val queen = new Queen()
+      val expectedSequence = "G7, F6, E5, D4, C3, B2, A1, H1, H2, H3, H4, H5, H6, H7, A8, B8, C8, D8, E8, F8, G8"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('H', 8), new ChessBoard) mustEqual (expectedMoves)
+    }
+    "be able to move only down right Diagonally and top and left boundary  when it is placed at left top corner" in {
+      val queen = new Queen()
+      val expectedSequence = "B7, C6, D5, E4, F3, G2, H1, A1, A2, A3, A4, A5, A6, A7, B8, C8, D8, E8, F8, G8, H8"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('A', 8), new ChessBoard) mustEqual (expectedMoves)
+    }
+    "be able to move Diagonally, Vertically and Horizontally when it is place at some border" in {
+      val queen = new Queen()
+      val expectedSequence = "B7, A6, D7, E6, F5, G4, H3, C1, C2, C3, C4, C5, C6, C7, A8, B8, D8, E8, F8, G8, H8"
+      val expectedMoves = parseCells(expectedSequence)
+
+      queen.move(Cell('C', 8), new ChessBoard) mustEqual (expectedMoves)
     }
   }
 }
