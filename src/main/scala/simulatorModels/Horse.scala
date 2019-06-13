5771b995e608e9a09d9c0ca40f2818c607483a1e
Tabish <tabish.shaikh@springernature.com>
2019-06-13 16:50:09 +0530

    Completed the solution by adding moves for Horse.

--- src/main/scala/simulatorModels/Horse.scala
+++ src/main/scala/simulatorModels/Horse.scala
@@ -2,6 +2,22 @@ package simulatorModels
 
 class Horse extends Piece {
   override def move(initialPosition: Cell, board: ChessBoard): List[Cell] = {
-    List.empty
+    val columnID = initialPosition.columnId
+    val rowID = initialPosition.rowId
+
+    val adjacentCell = 1
+    val thirdCell = 3
+    val upLeft = Cell((columnID - adjacentCell).toChar.toString + (rowID + thirdCell))
+    val upRight = Cell((columnID + adjacentCell).toChar.toString + (rowID + thirdCell))
+    val rightDown = Cell((columnID - adjacentCell).toChar.toString + (rowID - thirdCell))
+    val rightUp = Cell((columnID + adjacentCell).toChar.toString + (rowID - thirdCell))
+    val downLeft = Cell((columnID - thirdCell).toChar.toString + (rowID - adjacentCell))
+    val downRight = Cell((columnID - thirdCell).toChar.toString + (rowID + adjacentCell))
+    val leftDown = Cell((columnID + thirdCell).toChar.toString + (rowID - adjacentCell))
+    val leftUp = Cell((columnID + thirdCell).toChar.toString + (rowID + adjacentCell))
+
+    List(upLeft, upRight, rightDown, rightUp, downLeft, downRight, leftDown, leftUp)
+      .filter(_ != None)
+      .map(_.get)
   }
 }
